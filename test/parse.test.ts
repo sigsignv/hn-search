013d06eca8ef0636ca0bc42716a81b97f584e3ce
Sigsign <sig@signote.cc>
2025-04-22 11:13:44 +0900

    Export functions and types from index.ts

--- test/parse.test.ts
+++ test/parse.test.ts
@@ -1,14 +1,11 @@
 import { beforeAll, describe, expect, it } from "vitest";
-import { search } from "../src/index.ts";
 import { parse } from "../src/parse.ts";
 
 describe("parse() with actual search API Response data", () => {
   let response: Response;
 
   beforeAll(async () => {
-    response = await search({
-      query: "hackernews",
-    });
+    response = await fetch("https://hn.algolia.com/api/v1/search?tags=front_page");
   });
 
   it("should parse the response correctly", async () => {
