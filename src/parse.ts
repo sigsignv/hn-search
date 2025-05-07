6f65e480407a5dc26ce471dcf9cbc30233e41298
Sigsign <sig@signote.cc>
2025-05-07 18:32:34 +0900

    Replace initial parser

--- src/parse.ts
+++ src/parse.ts
@@ -1,37 +1,7 @@
-import * as v from "valibot";
+import type { AlgoliaSearchResult } from "./types.js";
+import { validateSearchResult } from "./validate.js";
 
-const IntegerSchema = v.pipe(v.number(), v.integer());
-
-const MatchedContentSchema = v.object({
-  _tags: v.array(v.string()),
-  author: v.string(),
-  children: v.optional(v.array(IntegerSchema)),
-  created_at: v.pipe(v.string(), v.isoTimestamp()),
-  num_comments: IntegerSchema,
-  points: IntegerSchema,
-  story_id: IntegerSchema,
-  title: v.string(),
-  updated_at: v.pipe(v.string(), v.isoTimestamp()),
-  url: v.optional(v.pipe(v.string(), v.url())),
-});
-
-const SearchResultSchema = v.object({
-  hits: v.array(MatchedContentSchema),
-  hitsPerPage: IntegerSchema,
-  nbHits: IntegerSchema,
-  nbPages: IntegerSchema,
-  page: IntegerSchema,
-  params: v.pipe(
-    v.string(),
-    v.transform((value) => new URLSearchParams(value)),
-  ),
-  query: v.string(),
-});
-
-export type SearchResult = v.InferOutput<typeof SearchResultSchema>;
-export type MatchedContent = v.InferOutput<typeof MatchedContentSchema>;
-
-export function parse(json: unknown): SearchResult {
-  const result = v.parse(SearchResultSchema, json);
+export function parse(json: unknown): AlgoliaSearchResult {
+  const result = validateSearchResult(json);
   return result;
 }
