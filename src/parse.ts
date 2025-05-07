a34ec73724f278237b2490f370cb15aef7656e4e
Sigsign <sig@signote.cc>
2025-05-07 18:44:57 +0900

    Remove initial parser

--- src/parse.ts
+++
@@ -1,7 +0,0 @@
-import type { AlgoliaSearchResult } from "./types.js";
-import { validateSearchResult } from "./validate.js";
-
-export function parse(json: unknown): AlgoliaSearchResult {
-  const result = validateSearchResult(json);
-  return result;
-}
