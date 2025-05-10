ea9f9d7d852ef3e2bacc58e30bdf5dfe6dbd8408
Sigsign <sig@signote.cc>
2025-05-10 23:21:29 +0900

    Add hnSearch() to replace old search functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { type SearchOptions, searchByDate, searchByRelevance } from "./search.js";
+
 export type { HttpClient } from "./client.js";
 
 export type { HackerNewsFilter } from "./filter.js";
@@ -6,3 +8,20 @@ export { authorTag, storyTag } from "./tag.js";
 
 export * from "./search.js";
 export * from "./types.js";
+
+export type HackerNewsSearchOptions = SearchOptions & {
+  sort?: "date" | "relevance";
+};
+
+export async function hnSearch(options: HackerNewsSearchOptions) {
+  const { sort = "relevance", ...rest } = options;
+
+  switch (sort) {
+    case "date":
+      return searchByDate(rest);
+    case "relevance":
+      return searchByRelevance(rest);
+    default:
+      throw new Error(`Unknown sort: ${sort}`);
+  }
+}
