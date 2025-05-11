1969e8071ec007b9c71767bb49e44111a88b3ef7
Sigsign <sig@signote.cc>
2025-05-12 00:16:58 +0900

    Restrict export types

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import { type SearchOptions, searchByDate, searchByRelevance } from "./search.js";
+import type { HackerNewsSearchResult } from "./types.js";
 
 export type { HttpClient } from "./client.js";
-
 export type { HackerNewsFilter } from "./filter.js";
 export type { HackerNewsTag } from "./tag.js";
 export { authorTag, storyTag } from "./tag.js";
 
 export * from "./search.js";
-export * from "./types.js";
+
+export type {
+  HackerNewsSearchResult,
+  HackerNewsStory,
+  HackerNewsComment,
+  HackerNewsPoll,
+  HackerNewsPollOption,
+} from "./types.js";
 
 export type HackerNewsSearchOptions = SearchOptions & {
   sort?: "date" | "relevance";
 };
 
-export async function hnSearch(options: HackerNewsSearchOptions) {
+export async function hnSearch(options: HackerNewsSearchOptions): Promise<HackerNewsSearchResult> {
   const { sort = "relevance", ...rest } = options;
 
   switch (sort) {
