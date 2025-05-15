ac713bc8f1aa1e0a282fe7ed3b231edfeb36e046
Sigsign <sig@signote.cc>
2025-05-16 00:52:31 +0900

    Add tests for buildFilterQueryString()

--- test/index.test.ts
+++ test/index.test.ts
@@ -21,7 +21,7 @@ describe("example", () => {
 });
 
 describe("buildFilterQueryString", () => {
-  it("Should build filter param string from multiple filter conditions", async ({ expect }) => {
+  it("should return a query string for multiple filter conditions", async ({ expect }) => {
     const filters: HackerNewsFilter[] = [
       ["created_at_i", "<", 1700000000],
       ["points", ">", 100],
@@ -29,5 +29,17 @@ describe("buildFilterQueryString", () => {
     ];
     const param = buildFilterQueryString(filters);
     expect(param).toBe("created_at_i<1700000000,points>100,num_comments>=10");
+  });
+
+  it("should return a query string for a single filter condition", async ({ expect }) => {
+    const filters: HackerNewsFilter[] = [["created_at_i", "<", 1700000000]];
+    const param = buildFilterQueryString(filters);
+    expect(param).toBe("created_at_i<1700000000");
+  });
+
+  it("should return an empty string for no filter conditions", async ({ expect }) => {
+    const filters: HackerNewsFilter[] = [];
+    const param = buildFilterQueryString(filters);
+    expect(param).toBe("");
   });
 });
